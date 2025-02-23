503c689fd80643339c0a1714142b61dbf32cae27
arielpinchover <59849910+arielpinchover@users.noreply.github.com>
2025-02-23 21:34:05 +0200

    added a caching mechanism

--- apps/api/src/rate.module.ts
+++ apps/api/src/rate.module.ts
@@ -5,9 +5,15 @@ import { RateService } from './rate.service';
 import { RateController } from './rate.controller';
 import { HttpModule } from '@nestjs/axios';
 import { SharedServicesModule } from '@app/shared-services';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [SharedServicesModule, HttpModule.register({ timeout: 5000, maxRedirects: 5 }), ScheduleModule.forRoot()],
+  imports: [SharedServicesModule, 
+    CacheModule.register({
+      isGlobal: true, // Use in-memory caching
+      ttl: 60 * 1000, // Cache duration: 60 seconds
+    }),
+    HttpModule.register({ timeout: 5000, maxRedirects: 5 }), ScheduleModule.forRoot()],
   providers: [RateService],
   controllers: [RateController],
   exports: [RateService], // Export if used in other modules
--- apps/api/src/rate.service.ts
+++ apps/api/src/rate.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Cron } from '@nestjs/schedule';
 import { lastValueFrom } from 'rxjs';
 import * as fs from 'fs';
 import * as path from 'path';
 import { FileService } from '@app/shared-services/file.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 const DATA_FILE = path.join(__dirname, 'rates.json');
 const COINGECKO_API = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether&vs_currencies=usd';
@@ -14,31 +16,56 @@ export class RateService {
   private readonly ratesFile = 'rates.json';
   private readonly logger = new Logger(RateService.name);
 
-  constructor(private readonly httpService: HttpService, private readonly fileService: FileService) {}
+  constructor(private readonly httpService: HttpService, private readonly fileService: FileService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
+  ) {}
 
   public async getRates(): Promise<any> {
-    return await this.fileService.readJsonFile(this.ratesFile);
+    const cachedRates = await this.cacheManager.get(this.ratesFile);
+    if (cachedRates) {
+      return cachedRates; // Return cached data if available
+    }
+
+    var rates = await this.fileService.readJsonFile(this.ratesFile);
+    if (rates) {
+      await this.cacheManager.set(this.ratesFile, rates, 60 * 1000); // Cache for 60 seconds
+    }
+    return rates;
 
   }
 
   private async saveRates(newRates: any): Promise<void> {
     await this.fileService.writeJsonFile(this.ratesFile, newRates);
+    await this.cacheManager.set(this.ratesFile, newRates, 60 * 1000);
   }
 
   async fetchRates(): Promise<any> {
     try {
       const response = await lastValueFrom(this.httpService.get(COINGECKO_API));
       const rates = response.data;
+  
+      // Save to file and cache
       await this.saveRates(rates);
+      await this.cacheManager.set('rates', rates, 60 * 1000);
+  
       return rates;
     } catch (error) {
       this.logger.error('Failed to fetch rates', error);
+  
+      // Try to get from cache first
+      const cachedRates = await this.cacheManager.get('rates');
+      if (cachedRates) {
+        return cachedRates;
+      }
+  
+      // If cache is empty, fallback to file
       return this.getRates();
     }
   }
+  
 
-  // @Cron('0 */10 * * * *') // Run every 10 minutes
-  @Cron('*/5 * * * * *') // Run every 5 seconds
+   @Cron('0 */10 * * * *') // Run every 10 minutes
+ // @Cron('*/5 * * * * *') // Run every 5 seconds
   async updateRates() {
     this.logger.log('Updating cryptocurrency rates...');
     await this.fetchRates();
